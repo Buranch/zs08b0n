7831e206fcbaebdbce14be62f3062e8f60df2d07
Buranch <birukmisanaw@gmail.com>
2019-06-06 03:13:57 +0300

    Fixing standard style errors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var name = require('./package.json').name;
-require('productionize')(name);
-const server = require('./lib/server');
-const port = process.env.PORT || 5100;
-server().listen(port);
-console.log(name, 'Server listening on port', port);
+var name = require('./package.json').name
+require('productionize')(name)
+const server = require('./lib/server')
+const port = process.env.PORT || 5100
+server().listen(port)
+console.log(name, 'Server listening on port', port)
